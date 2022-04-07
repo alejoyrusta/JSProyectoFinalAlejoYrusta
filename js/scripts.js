bc03aaf88d232130f4334ffd606897bf629b2a45
alejoyrusta <alejoyrusta@hotmail.com>
2022-04-06 22:11:26 -0300

    json

--- js/scripts.js
+++ js/scripts.js
@@ -1,107 +1,29 @@
-if (document.readyState == 'loading'){
-    document.addEventListener('DOMContentLoaded', ready)
-}else {
-    ready()
-}
-function ready(){
-    var sacarDelCarritoBoton = document.getElementsByClassName('btn-danger')
-    for (var i = 0; i < sacarDelCarritoBoton.length; i++) {
-    var button = sacarDelCarritoBoton[i]
-    button.addEventListener('click', function(event) {
-        var botonClickeado = event.target
-        botonClickeado.parentElement.parentElement.parentElement.parentElement.remove()
-    })
-    }
-    var botonAñadirAlCarrito = document.getElementsByClassName('btn btn-primary')
-    for ( let i = 0; i < botonAñadirAlCarrito.length; i++){
-        var button = botonAñadirAlCarrito[i]
-        button.addEventListener('click', añadirAlCarritoClick)
-    }   
-}
+let divProductos = document.getElementById('divProductos')
 
-function añadirAlCarritoClick(event) {
-    var button = event.target
-    var producto = button.parentElement.parentElement
-    var tipoProducto = producto.getElementsByClassName('tipo')[0].innerText
-    var marcaModeloProducto = producto.getElementsByClassName('marca')[0].innerText
-    var colorProducto = producto.getElementsByClassName('color')[0].innerText
-    var precioProducto = producto.getElementsByClassName('precio')[0].innerText
-    var imgProducto = producto.getElementsByClassName('card-img-top')[0].src
-    console.log(tipoProducto, marcaModeloProducto, colorProducto, precioProducto, imgProducto)
-    añadirAlCarrito(tipoProducto, marcaModeloProducto, colorProducto, precioProducto, imgProducto)
-}
-function añadirAlCarrito(tipoProducto, marcaModeloProducto, colorProducto, precioProducto, imgProducto){
-    var lineaDelCarrito = document.createElement('div')
-    lineaDelCarrito.classList.add('card-mb-3')
-    var productoEnCarrito = document.getElementsByClassName('carrito')[0]
-    var contenidoLineaCarrito =`
-                    <div class="row g-0">
-                        <div class="col-md-4" style="width: 12rem;">
-                        <img src="${imgProducto}" class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                        <div class="card-body carritoCardBody">
-                            <h5 class="card-title">${tipoProducto}</h5>
-                            <ul class="list-group list-group-flush">
-                            <li class="list-group-item marca cCBC">${marcaModeloProducto}</li>
-                            <li class="list-group-item color cCBC">${colorProducto}</li>
-                            <li class="list-group-item precio cCBC">${precioProducto}</li>
-                            </ul>
-                            <button type="button" class="btn btn-danger">Quitar</button>
-                            <a class="btn btn-success" href="/paginas/pago.html" role="button">Comprar</a>
-                        </div>
-                        </div>
-                    </div>`
-    lineaDelCarrito.innerHTML = contenidoLineaCarrito
-    productoEnCarrito.append(lineaDelCarrito)
-    lineaDelCarrito.getElementsByClassName('btn-danger')[0].addEventListener('click', botonClickeado)
-}
-
-(function () {
-    'use strict'
-    var forms = document.querySelectorAll('.needs-validation')
-    Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-            if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-            }
-            form.classList.add('was-validated')
-        }, false)
-        })
-})()
-document.getElementById('botonCompra').addEventListener('click',()=>{
-    const swalWithBootstrapButtons = Swal.mixin({
-        customClass: {
-            confirmButton: 'btn btn-success',
-            cancelButton: 'btn btn-danger'
-        },
-        buttonsStyling: false
-    })
-    swalWithBootstrapButtons.fire({
-        title: 'Esta seguro?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Si, Comprar!',
-        cancelButtonText: 'No, cancelar!',
-        reverseButtons: true
-    }).then((result) => {
-        if (result.isConfirmed) {
-            swalWithBootstrapButtons.fire(
-            'Comprado',
-            'La compra se a realizado con exito',
-            'success'
-            )
-        } else if (
-            result.dismiss === Swal.DismissReason.cancel
-        ) {
-        swalWithBootstrapButtons.fire(
-            'Compra cancelada',
-            '',
-            'error'
-        )
-        }
-    })
+fetch('./json/productos.json')
+.then(promesa => promesa.json())
+.then(data => {
+    data.forEach((producto, indice) => {
+        divProductos.innerHTML += `
+        <div>
+            <div class="card" id="producto${indice}" style="width: 18rem;">
+                <img src="/multimedia/imagenesProductos/${producto.imagen}" class="card-img-top" alt="${producto.modelo}">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.tipo}</h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item marca">Marca ${producto.marca}</li>
+                    <li class="list-group-item modelo">Modelo ${producto.modelo}</li>
+                    <li class="list-group-item color">Color ${producto.color}</li>
+                    <li class="list-group-item stock">Stock: ${producto.stock}</li>
+                    <li class="list-group-item precio">Precio $${producto.precio}</li>
+                </ul>
+                <div class="card-body"> 
+                    <a class="btn btn-primary" href="/paginas/pago.html" role="button">Comprar</a>
+                    <button class="btn btn-primary" id="boton${indice} type="button">Añadir al carrito</button>
+                </div>
+            </div>
+        </div>
+        `
+    });
 })
-
